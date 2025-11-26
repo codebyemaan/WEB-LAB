73e3e44cca37b73e7893e146406ed5bf97103701
eman saifullah <emansaifullah84@gmail.com>
2025-11-26 15:09:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("message").innerText = "hello world";
+document.getElementById("message1").innerText = "welcome to java script";
+function sum() {
+    let num1 = 20;
+    let num2 = 30;
+
+    let total = num1 + num2;
+
+   document.getElementById("result").innerText = "the sum is : " + total;
+}
+
+ 
